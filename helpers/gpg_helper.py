e8700e6a34ecad413e701e68733cab51085df081
fernando <fernando.gmp@gmail.com>
2017-12-01 15:26:54 -0200

    change in cat dir

--- helpers/gpg_helper.py
+++ helpers/gpg_helper.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import gnupg
 import os
 class GpgHelper:
     
     def __init__(self, directory=None):
-        self.directory = directory
-        os.system('rm -rf '+self.directory+'/gpghome')
+        self.directory = directory+'/gpghome'
+        os.system('rm -rf '+self.directory)
 
     def generate_key(self, email, passphrase):
         """
@@ -15,7 +14,7 @@ class GpgHelper:
         """
         gpg = None
         if not self.directory is None:
-            gpg = gnupg.GPG(gnupghome=self.directory+'/gpghome')
+            gpg = gnupg.GPG(gnupghome=self.directory)
 
         input_data = gpg.gen_key_input(name_email=email, passphrase=passphrase)
         key = gpg.gen_key(input_data)
@@ -27,7 +26,7 @@ class GpgHelper:
         """
         gpg = gnupg.GPG()
         if not self.directory is None:
-            gpg = gnupg.GPG(gnupghome=self.directory+'/gpghome')
+            gpg = gnupg.GPG(gnupghome=self.directory)
 
         ascii_armored_public_keys = gpg.export_keys(str(key))
         ascii_armored_private_keys = gpg.export_keys(str(key), True)
